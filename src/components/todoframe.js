146736464ebb197cbc9dd9f2c475bd3e9d1da128
lhj0621 <lhj0621@gmail.com>
2020-10-06 23:12:16 +0900

    frame 컴포넌트 제작

---
+++ src/components/todoframe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './todoframe.css';
+
+const TodoFrame = ({text}) => {
+  return (
+    <>
+      <div className="title">
+        <div>오늘 할 일</div>
+      </div>
+        
+    </>
+  );
+};
+
+export default TodoFrame;
