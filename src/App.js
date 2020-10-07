20bbe4f8386c6b620dc9cf45e39e08eb3b32a34c
lhj0621 <lhj0621@gmail.com>
2020-10-07 18:23:23 +0900

    todolist제작

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import TodoFrame from './components/todoframe';
 import TodoHead from './components/todohead';
+import Todolist from './components/todolist';
 function App() {
   return (
     <TodoFrame>
       <TodoHead/>
+      <Todolist/>
     </TodoFrame>
   
   );
---
+++ src/components/todolist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './todolist.css';
+
+function Todolist() {
+    return (
+        <div className ="todolist">
+            나는 리스트
+        </div>
+    );
+  }
+  
+  export default Todolist;
